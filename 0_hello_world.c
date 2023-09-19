346f0c1ea171ebcf6c19c9c7b086efe54de2de85
Eghosa Alex <alexsparks007@gmail.com>
2023-09-19 13:17:54 -0700

    beautiful code

---
+++ 0_hello_world.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints "Hello, World!" to the standard output.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Hello, World!\n");
+    return (0);
+}
